5622095b7e2e5a86414a4ac411a11c1de6539d39
Jessica Barnett <jessba2@pdx.edu>
2019-07-01 14:06:31 -0700

    added hello world code

---
+++ asciilines.cpp
@@ -0,0 +1,17 @@
+//Jessica Barnett
+//July 1st, 2019
+//asciilines.cpp
+
+#include <cstdlib>
+#include <cctype>
+#include <cstring>
+#include <iostream>
+#include <fstream>
+
+using namespace std;
+
+int main()
+{
+				cout<< "Hello World" <<endl;
+				return 0;
+}
---
+++ asciilines.h
@@ -0,0 +1,5 @@
+//Jessica Barnett
+//July 1st, 2019
+//asciilines.h
+
+
